698155149ae13befb5647c261413305e164cf171
Christoph Kappestein <christoph.kappestein@gmail.com>
2021-07-31 14:32:08 +0200

    update copy

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,16 @@
 module.exports = function(grunt){
   grunt.initConfig({
-    concat: {
-      dist_js: {
-        options: {
-          separator: ';\n',
-          process: function(src, filepath) {
-            return '// Source: ' + filepath + '\n' +
-              src.replace(/\/\/# sourceMappingURL=([A-z0-9\-\.\_]+)/g, '').trim();
-          }
-        },
-        src: [
-          './generated/*.js',
-          './dist/src/*.js',
-          './dist/index.js',
-        ],
-        dest: './dist/worker.js'
+    copy: {
+      main: {
+        expand: true,
+        src: 'generated/*',
+        dest: 'dist/',
       },
-    },
+    }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerTask('default', ['concat']);
+  grunt.registerTask('default', ['copy']);
 
 };
