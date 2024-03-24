0cc5475e71990c0bb1f70d0ea02b6929326c1184
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-03-24 10:25:51 +0100

    update

--- src/Connector.ts
+++ src/Connector.ts
@@ -7,29 +7,29 @@ import {ExecuteConnection} from "./generated/ExecuteConnection";
 
 export class Connector {
 
-    private configs: Record<string, ExecuteConnection>;
-    private connections: Record<string, any>;
+    private connections: Record<string, ExecuteConnection>;
+    private instances: Record<string, any>;
 
-    public constructor(configs: Record<string, ExecuteConnection>) {
-        this.configs = configs;
-        this.connections = {};
+    public constructor(connections: Record<string, ExecuteConnection>) {
+        this.connections = connections;
+        this.instances = {};
     }
 
     public async getConnection(name: string): Promise<Connection|PgClient|AxiosInstance|MongoClient|ElasticsearchClient> {
-        if (this.connections[name]) {
-            return this.connections[name];
+        if (this.instances[name]) {
+            return this.instances[name];
         }
 
-        if (!this.configs[name]) {
+        if (!this.connections[name]) {
             throw new Error('Connection does not exist');
         }
 
-        const connection = this.configs[name];
+        const connection = this.connections[name];
         const config = JSON.parse(Buffer.from(connection.config || '', 'base64').toString());
 
         if (connection.type === 'Fusio.Adapter.Sql.Connection.Sql') {
             if (config.type === 'pdo_mysql') {
-                return this.connections[name] = createConnection({
+                return this.instances[name] = createConnection({
                     host: config.host,
                     user: config.username,
                     password: config.password,
@@ -43,33 +43,33 @@ export class Connector {
                     database: config.database
                 });
                 await client.connect();
-                this.connections[name] = client;
+                this.instances[name] = client;
                 return client;
             } else {
                 throw new Error('SQL type is not supported');
             }
         } else if (connection.type === 'Fusio.Adapter.Sql.Connection.SqlAdvanced') {
-            return this.connections[name] = createConnection(config.url);
+            return this.instances[name] = createConnection(config.url);
         } else if (connection.type === 'Fusio.Adapter.Http.Connection.Http') {
             // @TODO configure proxy for http client
             //config.username
             //config.password
             //config.proxy
 
-            return this.connections[name] = axios.create({
+            return this.instances[name] = axios.create({
                 baseURL: config.url
             });
         } else if (connection.type === 'Fusio.Adapter.Mongodb.Connection.MongoDB') {
             const client = new MongoClient(config.url);
             await client.connect();
 
-            return this.connections[name] = client;
+            return this.instances[name] = client;
         } else if (connection.type === 'Fusio.Adapter.Elasticsearch.Connection.Elasticsearch') {
             const client = new ElasticsearchClient({
                 nodes: config.host.split(',')
             });
 
-            return this.connections[name] = client;
+            return this.instances[name] = client;
         } else {
             throw new Error('Provided a not supported connection type');
         }
